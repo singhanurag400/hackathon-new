a47fdc96e167c7b94cc0d434529703a104df7f49
anurag kumar singh <singhanurag400@gmail.com>
2020-02-15 14:22:17 +0530

    firstCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,42 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { CartComponent } from './cart/cart.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ProductComponent } from './product/product.component';
+import { FormsModule, ReactiveFormsModule } from '../../node_modules/@angular/forms';
+import { ToastrModule } from '../../node_modules/ngx-toastr';
+// import { NgxSpinnerModule } from "ngx-spinner";
+import { HttpClientModule } from '../../node_modules/@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    CartComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomePageComponent,
+    ProductComponent
   ],
   imports: [
+    CommonModule,
+    BrowserAnimationsModule,    
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ToastrModule.forRoot(),
+    // NgxSpinnerModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
